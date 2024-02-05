babb50103ab7cab112a83b891b59526a210114d6
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-05 14:48:16 -0800

    msg for non-existent resources

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const { notFoundError } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+router.use((req, res) => {
+  res.status(notFoundError).send({
+    message: "Requested resource not found",
+  });
+});
 
 module.exports = router;
