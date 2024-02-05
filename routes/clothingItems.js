cda8496ccfc5b5a39f1efffc884bedbb4032dca6
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-05 10:30:48 -0800

    clothing items routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/", () => console.log("GET users"));
-router.get("/:userId", () => console.log("GET users by ID"));
-router.post("/", () => console.log("POST users"));
+router.get("/", () => console.log("GET items"));
+router.get("/:itemId", () => console.log("GET items by ID"));
+router.post("/", () => console.log("POST items"));
 
 module.exports = router;
