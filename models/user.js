a37cd893fd14f0852ce6337e8eb5b06e9c5807ac
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-18 13:26:43 -0800

    login controller

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
     required: true,
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, "Name must be at least 2 characters"],
+    maxlength: [30, "Name cannot exceed 30 characters"],
   },
   avatar: {
     required: true,
@@ -37,5 +37,24 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect Email Or Password"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect Email Or Password"));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
