9e3f66bf02d955757cb305f036b9e56d4be6884b
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-18 14:40:21 -0800

    fix PostMan errors

--- controllers/users.js
+++ controllers/users.js
@@ -14,6 +14,7 @@ const {
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
   User.findOne({ email })
+    .select("+password")
     .then((user) => {
       if (!email) {
         throw new Error("Enter A Valid Email Address");
@@ -34,8 +35,10 @@ const createUser = (req, res) => {
       if (err.name === "ValidationError") {
         return res.status(invalidDataError).send({ message: "Invalid Data" });
       }
-      if (err.name === "CastError") {
-        return res.status(invalidDataError).send({ message: "Invalid Data" });
+      if (err.message === "Enter A Valid Email Address") {
+        return res
+          .status(invalidDataError)
+          .send({ message: "Invalid Credentials" });
       }
       if (err.message === "Email Already In Use") {
         return res.status(conflictError).send({
@@ -59,9 +62,8 @@ const getCurrentUser = (req, res) => {
     .catch((err) => {
       if (err.message === "User Not Found") {
         return res.status(notFoundError).send({ message: "User Not Found" });
-      } 
-        return res.status(serverError).send({ message: "Server Error" });
-      
+      }
+      return res.status(serverError).send({ message: "Server Error" });
     });
 };
 
@@ -95,23 +97,21 @@ const updateCurrentUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res
-      .staus(invalidDataError)
-      .send({ message: "Incorrect Email Or Password" });
+    res.staus(invalidDataError).send({ message: "Invalid Credentials" });
+    return;
   }
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: "7d",
       });
-      res.status(200).send({ data: token });
+      // res.status(200).send({ data: token });
+      res.send({ token });
     })
     .catch((err) => {
       console.error(err);
       if (err.message === "Incorrect Email Or Password") {
-        res
-          .status(unauthorizedError)
-          .send({ message: "Incorrect Email Or Password" });
+        res.status(unauthorizedError).send({ message: "Invalid Credentials" });
       } else {
         res.status(serverError).send({ message: "Server Error" });
       }
