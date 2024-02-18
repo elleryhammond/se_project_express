96596e7fe69a0b8eb96011b71480817a8b5c9d16
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-18 10:17:49 -0800

    select:false added to password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+// const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,7 +33,8 @@ const userSchema = new mongoose.Schema({
   password: {
     required: true,
     type: String,
-    // select: false,
+    minlength: 8,
+    select: false,
   },
 });
 
