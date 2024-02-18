a933a2aefbe4bc0b38e40aa839b36ee6a9b20eb2
Ellery Hammond <elleryclairehammond@gmail.com>
2024-02-18 13:09:56 -0800

    add login and signup routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { notFoundError } = require("../utils/errors");
+const { login, createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use((req, res) => {
   res.status(notFoundError).send({
