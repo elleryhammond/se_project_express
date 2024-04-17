fb2c273e6d11d28627f061db167f903d1b6bd275
Ellery Hammond <elleryclairehammond@gmail.com>
2024-04-16 19:11:48 -0700

    update env/secret key

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,13 @@
-const JWT_SECRET = "SecretKey123";
+// const JWT_SECRET = "SecretKey123";
 
-module.exports = { JWT_SECRET };
+// module.exports = { JWT_SECRET };
+
+// const { NODE_ENV, JWT_SECRET } = process.env;
+
+// const token = jwt.sign(
+//   { _id: user._id },
+//   NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+// );
+
+const { NODE_ENV, JWT_SECRET = "dev-secret" } = process.env;
+module.exports = { JWT_SECRET, NODE_ENV };
