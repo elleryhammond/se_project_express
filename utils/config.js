8dca74afa5dd48057c58c548ea3c22c85c8fd4e1
Ellery Hammond <elleryclairehammond@gmail.com>
2024-04-18 08:07:41 -0700

    remove/delete unused code

--- utils/config.js
+++ utils/config.js
@@ -1,13 +1,2 @@
-// const JWT_SECRET = "SecretKey123";
-
-// module.exports = { JWT_SECRET };
-
-// const { NODE_ENV, JWT_SECRET } = process.env;
-
-// const token = jwt.sign(
-//   { _id: user._id },
-//   NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
-// );
-
 const { NODE_ENV, JWT_SECRET = "dev-secret" } = process.env;
 module.exports = { JWT_SECRET, NODE_ENV };
